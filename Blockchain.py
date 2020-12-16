bdbf538cc42f85aa57dda467c21817b2fa563363
Nidarshan <nidarshans@gmail.com>
2020-12-15 16:57:27 -0800

    todo: add_block() method

--- Blockchain.py
+++ Blockchain.py
@@ -3,6 +3,15 @@ from datetime import datetime
 
 class Blockchain:
     def __init__(self):
-        self.genesis_block = Block(None, None, None)
+        self.genesis_block = Block(None, datetime.now(), '0')
+        genesis_block.generate_hash()
         self.last_block = genesis_block
-    
+    def add_block(self, block, proof):
+        """
+        A function that adds the block to the chain after verification.
+        Verification includes:
+        * Checking if the proof is valid.
+        * The previous_hash referred in the block and the hash of a latest block
+          in the chain match.
+        """
+        pass
