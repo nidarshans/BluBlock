d1847715b5ab02abe57d790b14c497318fbba12d
Nidarshan <nidarshans@gmail.com>
2020-12-15 16:55:58 -0800

    added add_transaction() func

--- Block.py
+++ Block.py
@@ -2,18 +2,22 @@ import json
 import MerkleTree
 
 class Block:
-    def __init__(self, data: str, timestamp: datetime, previous_hash: str):
+    def __init__(self, data: str, timestamp: datetime, previous_hash: str, previous_block: Block):
         """
         Constructor for a Block
-        @param id               The unique id for the block (SHA256)
-        @param data             The data stored in the block
+        @param data             The data stored in the block (JSON)
         @param timestamp        The timestamp
         @param previous_hash    The hash of the previous block
         """
-        self.id = self.generate_hash()
+        self.id = ''
         self.data = data
         self.timestamp = timestamp
         self.previous_hash = previous_hash
+        self.previous_block = previous_block
         self.nonce = '0x'
-        self.mtree = MerkleTree()
-        
+        self.mtree = MerkleTree(data)
+        self.hash = None
+    def generate_hash(self):
+        self.hash = mtree.compute_hash()
+    def add_transaction(self, data: str):
+        self.mtree.insert(data)
