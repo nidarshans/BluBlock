d2f6f2227055888267c1480d589824d4d48d3415
Nidarshan <nidarshans@gmail.com>
2020-12-23 16:02:27 -0800

    loads tx_pool from file

--- Blockchain.py
+++ Blockchain.py
@@ -2,7 +2,7 @@ from Block import Block
 from datetime import datetime
 
 class Blockchain:
-    difficulty = 10
+    difficulty = 3
     index = 0
     def __init__(self):
         self.genesis_block = Block(0, '__GENESIS__', datetime.now(), '0')
@@ -14,12 +14,10 @@ class Blockchain:
         block.nonce = hex(0)
         x = 0
         block.generate_hash()
-        '''
         while not block.hash.startswith('0' * Blockchain.difficulty):
             x += 1
             block.nonce = hex(x)
             block.generate_hash()
-        '''
         return block
     def add_block(self, block: Block, proof: str) -> bool:
         """
@@ -31,10 +29,8 @@ class Blockchain:
         """
         if self.chain[-1].hash != block.previous_hash:
             return False
-        '''
         if self.check_proof(block, proof) == False:
             return False
-        '''
         # block.hash = proof
         self.chain.append(block)
         Blockchain.index += 1
@@ -46,8 +42,11 @@ class Blockchain:
         """
         return (block_hash.startswith('0' * Blockchain.difficulty) and
                 block_hash == block.generate_hash())
-    def add_unconfirmed_transaction(self, transaction: str):
-        self.unconfirmed_transactions.append(transaction)
+    def load_unconfirmed_transaction(self):
+        with open('tx_pool.json', 'r') as pool:
+            for transaction in pool:
+                self.unconfirmed_transactions.append(transaction)
+        open('tx_pool.json', 'w').close()
     def mine(self) -> int:
         new_block = Block(Blockchain.index, self.unconfirmed_transactions,
                     datetime.now(), self.chain[-1].hash)
